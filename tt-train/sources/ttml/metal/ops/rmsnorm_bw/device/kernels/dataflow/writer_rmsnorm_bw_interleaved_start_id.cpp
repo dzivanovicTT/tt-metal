81f161ef2792cd8dd1c41f8b65faed46ca3bf20a
Maciek Draguła <mdragula@tenstorrent.com>
2025-06-09 14:35:23 +0000

    [WIP] Moved prints to writer, cleaned up the compute a but

--- tt-train/sources/ttml/metal/ops/rmsnorm_bw/device/kernels/dataflow/writer_rmsnorm_bw_interleaved_start_id.cpp
+++ tt-train/sources/ttml/metal/ops/rmsnorm_bw/device/kernels/dataflow/writer_rmsnorm_bw_interleaved_start_id.cpp
@@ -6,6 +6,47 @@
 
 #include "dataflow_api.h"
 
+// constexpr uint32_t num_rows_per_core = get_compile_time_arg_val(0);
+// constexpr uint32_t block_size =
+//     get_compile_time_arg_val(1);                          // Number of tiles in the inner dimention of the input
+//     tensor.
+// constexpr uint32_t mask_w = get_compile_time_arg_val(2);  // Unused atm.
+// constexpr uint32_t Wt = get_compile_time_arg_val(3);
+
+// Think about move this to compile args to ainline void mess while adjusting indicies
+//  CBs with input data
+constexpr uint32_t cb_input_idx = tt::CBIndex::c_0;
+constexpr uint32_t cb_mask_w_idx = tt::CBIndex::c_1;  // Unused atm
+constexpr uint32_t cb_scaler_idx = tt::CBIndex::c_2;
+constexpr uint32_t cb_gamma_idx = tt::CBIndex::c_3;  // Number of activations, i.e. c in the paper
+constexpr uint32_t cb_rms_a_idx = tt::CBIndex::c_4;
+constexpr uint32_t cb_dL_out_idx = tt::CBIndex::c_5;
+// CBs with output data
+// Create more intermedaite-output CBs that will be used exclusively by the writer. Do not compute anything on them
+constexpr uint32_t cb_dL_da_idx = tt::CBIndex::c_6;
+constexpr uint32_t cb_dL_dgamma_idx = tt::CBIndex::c_7;
+// CBs with intermediate computations
+constexpr uint32_t cb_scaled_gain = tt::CBIndex::c_8;
+constexpr uint32_t cb_gained_dL_dout = tt::CBIndex::c_9;
+constexpr uint32_t cb_scale = tt::CBIndex::c_10;
+constexpr uint32_t cb_ms_a = tt::CBIndex::c_11;
+constexpr uint32_t cb_c_by_ms_a = tt::CBIndex::c_12;
+constexpr uint32_t cb_rhs = tt::CBIndex::c_13;
+constexpr uint32_t cb_a_over_rms_a = tt::CBIndex::c_14;
+constexpr uint32_t cb_dL_dgamma_components = tt::CBIndex::c_15;
+
+constexpr uint32_t onetile = 1;
+
+inline void print_full_tile(uint32_t cb_id, uint32_t tile_id = 0, bool untilize = false) {
+    for (uint8_t r = 0; r < 32; ++r) {
+        SliceRange sr_left = SliceRange{.h0 = r, .h1 = (uint8_t)(r + 1), .hs = 1, .w0 = 0, .w1 = 16, .ws = 1};
+        SliceRange sr_right = SliceRange{.h0 = r, .h1 = (uint8_t)(r + 1), .hs = 1, .w0 = 16, .w1 = 32, .ws = 1};
+        DPRINT << (uint)r << ": " << TileSlice(cb_id, tile_id, sr_left, false, untilize) << " "
+               << TileSlice(cb_id, tile_id, sr_right, true, untilize) << ENDL();
+        break;
+    }
+}
+
 void kernel_main() {
     // DPRINT << "Starting writer" << ENDL();
     uint32_t runtime_args_counter = 0;
@@ -31,6 +72,26 @@ void kernel_main() {
 
     const InterleavedAddrGenFast</* is dram */ true> dgamma_output_addr_generator = {
         .bank_base_address = dgamma_output_addr, .page_size = tile_bytes, .data_format = data_format};
+
+    // cb_wait_front(cb_gained_dL_dout, 1);
+    // DPRINT << "cb_gained_dL_dout" << ENDL();
+    // print_full_tile(cb_gained_dL_dout, 0);
+
+    // cb_wait_front(cb_scale, 1);
+    // DPRINT << "cb_scale" << ENDL();
+    // print_full_tile(cb_scale, 0);
+
+    // cb_wait_front(cb_c_by_ms_a, 1);
+    // DPRINT << "cb_c_by_ms_a" << ENDL();
+    // print_full_tile(cb_c_by_ms_a, 0);
+
+    // cb_wait_front(cb_rhs, 1);
+    // DPRINT << "cb_rhs" << ENDL();
+    // print_full_tile(cb_rhs, 0);
+
+    cb_wait_front(cb_dL_da_idx, onetile);
+    DPRINT << "cb_dL_da_idx" << ENDL();
+    print_full_tile(cb_dL_da_idx, 0);
 
     uint32_t end_row = start_row + num_rows_to_process;
     // DPRINT << "Writing dx and dgamma for rows from " << start_row << " to " << end_row << ENDL();
