65dfc8abef577c0431bbecdddcdac687d79a2db1
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-06-09 08:59:04 +0000

    revert 2

--- tt_metal/impl/debug/inspector.cpp
+++ tt_metal/impl/debug/inspector.cpp
@@ -14,11 +14,7 @@
 namespace tt::tt_metal {
 
 static inspector::Data* get_inspector_data() {
-    auto* data = tt::tt_metal::MetalContext::instance().get_inspector_data();
-    if (!data) {
-        throw std::runtime_error("Inspector data is not initialized.");
-    }
-    return data;
+    return tt::tt_metal::MetalContext::instance().get_inspector_data();
 }
 
 bool Inspector::is_enabled() {
