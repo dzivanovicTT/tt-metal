6888f1e05f6648caee450888b0e274bf98df7e64
Pavle Glusac <pglusac@tenstorrent.com>
2025-06-10 15:59:00 +0000

    Enable multilink on unicast ag/rs

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/interleaved_dim3_1_1_any_any_receiver_reader.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/interleaved_dim3_1_1_any_any_receiver_reader.cpp
@@ -29,8 +29,6 @@ constexpr bool direction = get_compile_time_arg_val(8);
 constexpr uint32_t contig_pages_advanced = get_compile_time_arg_val(9);
 
 constexpr uint32_t N_DRAM_BANKS = 12;
-constexpr uint32_t my_chip_id_x = my_chip_id % N_DRAM_BANKS;
-constexpr uint32_t my_chip_id_y = my_chip_id / N_DRAM_BANKS;
 
 void kernel_main() {
     ///////////////////////////////////////////////////
@@ -39,9 +37,11 @@ void kernel_main() {
 
     uint32_t arg_idx = 0;
     address_t intermediate_buffer_addr = get_arg_val<address_t>(arg_idx++);
-    uint32_t slice_num_pages = get_arg_val<uint32_t>(arg_idx++);
+    uint32_t input_tile_id_start = get_arg_val<uint32_t>(arg_idx++);
+    uint32_t input_tile_id_end = get_arg_val<uint32_t>(arg_idx++);
     uint32_t ring_size = get_arg_val<uint32_t>(arg_idx++);
     size_t out_ready_sem = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+    uint32_t link = get_arg_val<uint32_t>(arg_idx++);
 
     constexpr bool intermediate_tensor_is_dram = intermediate_buffer_type == tt::tt_metal::BufferType::DRAM;
     auto intermediate_tensor_addrgen = InterleavedAddrGenFast<intermediate_tensor_is_dram>{
@@ -65,11 +65,7 @@ void kernel_main() {
         }
     }
 
-    const uint32_t payload_size_bytes = input_tensor_page_size * contig_pages_advanced;
-
     volatile tt_l1_ptr uint32_t* out_ready_sem_addr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(out_ready_sem);
-    uint32_t actual_sender_chip_id_x = my_chip_id_x;
-    uint32_t actual_sender_chip_id_y = my_chip_id_y;
     while (slices_received < slices_expected) {
         // Do i expect more?
         // If direction == backward, do I expect more from the left?
@@ -82,38 +78,40 @@ void kernel_main() {
         noc_semaphore_wait_min(out_ready_sem_addr, slices_received + 1);
         // Got it
         slices_received++;
+
+        int sender_chip_id;
+        uint32_t actual_sender_chip_id;
         if (direction == 1) {
-            actual_sender_chip_id_x = (actual_sender_chip_id_x == ring_size - 1) ? 0 : actual_sender_chip_id_x + 1;
+            sender_chip_id = my_chip_id + slices_received;
+            actual_sender_chip_id = (sender_chip_id >= (int)ring_size) ? sender_chip_id - ring_size : sender_chip_id;
         } else {
-            actual_sender_chip_id_x = (actual_sender_chip_id_x == 0) ? ring_size - 1 : actual_sender_chip_id_x - 1;
+            sender_chip_id = my_chip_id - slices_received;
+            actual_sender_chip_id = (sender_chip_id < 0) ? ring_size + sender_chip_id : sender_chip_id;
         }
 
-        uint32_t tiles_read = 0;
-        uint32_t tiles_to_read = slice_num_pages;
+        uint32_t tiles_read = input_tile_id_start;
+        uint32_t tiles_to_read = input_tile_id_end;
 
-        uint32_t packet_id = 0;
-        uint32_t intermediate_packet_id_x = actual_sender_chip_id_x;
-        uint32_t intermediate_packet_id_y = actual_sender_chip_id_y;
+        uint32_t packet_id =
+            ((input_tile_id_end - input_tile_id_start) + contig_pages_advanced - 1) / contig_pages_advanced * link;
         while (tiles_read < tiles_to_read) {
             uint32_t num_pages_to_read = std::min(tiles_to_read - tiles_read, packet_size_in_pages);
             cb_reserve_back(cb_intermediate_id, num_pages_to_read);
             size_t l1_write_addr = get_write_ptr(cb_intermediate_id);
             for (uint32_t j = 0; j < num_pages_to_read; j += contig_pages_advanced) {
+                const uint32_t payload_size_bytes =
+                    input_tensor_page_size * min(num_pages_to_read - j, contig_pages_advanced);
+                uint32_t intermediate_packet_id = actual_sender_chip_id + packet_id * ring_size;
                 uint32_t intermediate_packet_first_tile_id =
-                    intermediate_packet_id_x + contig_pages_advanced * N_DRAM_BANKS * intermediate_packet_id_y;
+                    (intermediate_packet_id % N_DRAM_BANKS) +
+                    contig_pages_advanced * N_DRAM_BANKS * (intermediate_packet_id / N_DRAM_BANKS);
                 uint64_t packet_addr = get_noc_addr(
                     intermediate_packet_first_tile_id, intermediate_tensor_addrgen, 0 /*offset*/, 0 /*noc_id*/);
 
                 noc_async_read(packet_addr, l1_write_addr, payload_size_bytes);
                 l1_write_addr += payload_size_bytes;
-                tiles_read += contig_pages_advanced;
+                tiles_read += min(num_pages_to_read - j, contig_pages_advanced);
                 packet_id++;
-
-                intermediate_packet_id_x += ring_size;
-                if (intermediate_packet_id_x >= N_DRAM_BANKS) {
-                    intermediate_packet_id_x -= N_DRAM_BANKS;
-                    intermediate_packet_id_y++;
-                }
             }
             noc_async_read_barrier();
             cb_push_back(cb_intermediate_id, num_pages_to_read);
@@ -121,4 +119,5 @@ void kernel_main() {
     }
 
     noc_semaphore_set(out_ready_sem_addr, 0);
+    DPRINT << "Done RECEIVER READER\n";
 }
