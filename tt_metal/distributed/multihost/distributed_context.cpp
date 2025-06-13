b1d6028ba8f6aa7d0363470123a0eaa1efb9b7c6
asaigal <asaigal@tenstorrent.com>
2025-06-13 20:41:46 +0000

    Init distributed context in metal context

--- tt_metal/distributed/multihost/distributed_context.cpp
+++ tt_metal/distributed/multihost/distributed_context.cpp
@@ -24,4 +24,15 @@ void DistributedContext::create(int argc, char** argv) { ContextImpl::create(arg
 const ContextPtr& DistributedContext::get_current_world() { return ContextImpl::get_current_world(); }
 
 void DistributedContext::set_current_world(const ContextPtr& ctx) { ContextImpl::set_current_world(ctx); }
+
+bool DistributedContext::is_initialized() { return ContextImpl::is_initialized(); }
+
+bool DistributedContext::using_mpi_environment() {
+    // `mpirun` and other MPI runners set predictable env vars and can use
+    // this to detect if we are in an MPI environment
+    return (std::getenv("OMPI_COMM_WORLD_SIZE") != nullptr);
+}
+
 }  // namespace tt::tt_metal::distributed::multihost
+
+
