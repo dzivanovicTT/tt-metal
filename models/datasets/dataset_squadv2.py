a46d51307eae2e57d9ae52006d6fa3e9bd154837
Kannika Kabilar <kkabilar@tenstorrent.com>
2025-06-13 16:13:10 -0500

    cleanup 2

--- models/datasets/dataset_squadv2.py
+++ models/datasets/dataset_squadv2.py
@@ -99,10 +99,10 @@ def squad_divide_chunks(dataset_question, dataset_context, dataset_reference, ba
 
 
 def squadv2_1K_samples_input(tokenizer, seq_len, attention_mask, token_type_ids, microbatch=8):
-    path = model_location_generator2_("squad_v2")
-    #squadv2_dataset = load_from_disk("/mnt/MLPerf/tt_dnn-models/squadv2")
-    squadv2_dataset = load_dataset(path, use_auth_token=False)["validation"]
-    squadv2_dataset.save_to_disk("/mnt/MLPerf/tt_dnn-models/squad_v2")
+    #path = model_location_generator2_("squad_v2")
+    squadv2_dataset = load_from_disk(model_location_generator2_("squad_v2"))
+    #squadv2_dataset = load_dataset("squad_v2", use_auth_token=False)["validation"]
+    #squadv2_dataset.save_to_disk("/mnt/MLPerf/tt_dnn-models/squad_v2")
     #my_dataset = load_dataset(path, use_auth_token=False)["validation"]
     #my_dataset.save_to_disk("/mnt/MLPerf/tt_dnn-models/squadv2")
     # squadv2_dataset = load_dataset("squad_v2", use_auth_token=True, streaming=True)["validation"]
