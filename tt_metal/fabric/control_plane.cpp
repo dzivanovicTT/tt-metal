b0d50603ba6f403043ce13df3d54aea3d920c6b8
Sean Nijjar <sean.nijjar@gmail.com>
2025-06-17 17:36:26 +0000

    finally fixed?

--- tt_metal/fabric/control_plane.cpp
+++ tt_metal/fabric/control_plane.cpp
@@ -183,7 +183,7 @@ void ControlPlane::initialize_dynamic_routing_plane_counts(
 
         // First pass: Calculate minimums for each row/column
         size_t num_chips_in_mesh = intra_mesh_connectivity[mesh_id.get()].size();
-        bool may_have_intra_mesh_connectivity = num_chips_in_mesh > 1;
+        bool may_have_intra_mesh_connectivity = true;  // num_chips_in_mesh > 1;
         if (may_have_intra_mesh_connectivity) {
             for (std::uint32_t chip_id = 0; chip_id < num_chips_in_mesh; chip_id++) {
                 const auto fabric_node_id = FabricNodeId(MeshId{mesh_id}, chip_id);
