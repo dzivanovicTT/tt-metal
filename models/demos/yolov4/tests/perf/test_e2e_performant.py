c1d0f195cb8c0a4d9a55beba4a8f6d55043fd88f
kkeerthana0573 <kola.keerthana@multicorewareinc.com>
2025-06-27 10:23:06 +0000

    #24283: Add DP Support for YoloV4

--- models/demos/yolov4/tests/perf/test_e2e_performant.py
+++ models/demos/yolov4/tests/perf/test_e2e_performant.py
@@ -9,8 +9,55 @@ import torch
 from loguru import logger
 
 import ttnn
+from models.demos.yolov4.common import get_mesh_mappers
 from models.demos.yolov4.runner.performant_runner import YOLOv4PerformantRunner
 from models.utility_functions import run_for_wormhole_b0
+
+
+def run_perf_e2e_yolov4(
+    device,
+    batch_size_per_device,
+    act_dtype,
+    weight_dtype,
+    model_location_generator,
+    resolution,
+):
+    inputs_mesh_mapper, _, output_mesh_composer = get_mesh_mappers(device)
+
+    performant_runner = YOLOv4PerformantRunner(
+        device,
+        batch_size_per_device,
+        act_dtype,
+        weight_dtype,
+        resolution=resolution,
+        model_location_generator=None,
+        mesh_mapper=inputs_mesh_mapper,
+        mesh_composer=output_mesh_composer,
+    )
+
+    num_devices = device.get_num_devices()
+    batch_size = batch_size_per_device * num_devices
+
+    input_shape = (batch_size_per_device, 3, *resolution)
+    torch_input_tensor = torch.randn(input_shape, dtype=torch.float32)
+
+    for i in range(batch_size - 1):
+        torch_input_tensor = torch.cat([torch_input_tensor] * batch_size, dim=0)
+
+    ttnn_input_tensor = ttnn.from_torch(torch_input_tensor, ttnn.bfloat16, mesh_mapper=inputs_mesh_mapper)
+
+    inference_times = []
+    for _ in range(10):
+        t0 = time.time()
+        _ = performant_runner.run(torch_input_tensor)
+        t1 = time.time()
+        inference_times.append(t1 - t0)
+
+    performant_runner.release()
+    inference_time_avg = sum(inference_times) / len(inference_times)
+    logger.info(
+        f"Model: ttnn_yolov4 - batch_size: {batch_size}, resolution: {resolution}. One inference iteration time (sec): {inference_time_avg}, FPS: {(batch_size * num_devices)/inference_time_avg}"
+    )
 
 
 @run_for_wormhole_b0()
@@ -19,7 +66,7 @@ from models.utility_functions import run_for_wormhole_b0
     "device_params", [{"l1_small_size": 40960, "trace_region_size": 6434816, "num_command_queues": 2}], indirect=True
 )
 @pytest.mark.parametrize(
-    "batch_size, act_dtype, weight_dtype",
+    "batch_size_per_device, act_dtype, weight_dtype",
     ((1, ttnn.bfloat16, ttnn.bfloat16),),
 )
 @pytest.mark.parametrize(
@@ -31,34 +78,51 @@ from models.utility_functions import run_for_wormhole_b0
 )
 def test_e2e_performant(
     device,
-    batch_size,
+    batch_size_per_device,
     act_dtype,
     weight_dtype,
     model_location_generator,
     resolution,
 ):
-    performant_runner = YOLOv4PerformantRunner(
+    run_perf_e2e_yolov4(
         device,
-        batch_size,
+        batch_size_per_device,
         act_dtype,
         weight_dtype,
-        resolution=resolution,
-        model_location_generator=None,
+        model_location_generator,
+        resolution,
     )
 
-    inference_times = []
-    for _ in range(10):
-        input_shape = (1, 3, *resolution)
-        torch_input_tensor = torch.randn(input_shape, dtype=torch.float32)
 
-        t0 = time.time()
-        _ = performant_runner.run(torch_input_tensor)
-        t1 = time.time()
-        inference_times.append(t1 - t0)
-
-    performant_runner.release()
-
-    inference_time_avg = round(sum(inference_times) / len(inference_times), 6)
-    logger.info(
-        f"ttnn_yolov4_batch_size: {batch_size}, resolution: {resolution}. One inference iteration time (sec): {inference_time_avg}, FPS: {round(batch_size/inference_time_avg)}"
+@run_for_wormhole_b0()
+@pytest.mark.models_performance_bare_metal
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 40960, "trace_region_size": 6434816, "num_command_queues": 2}], indirect=True
+)
+@pytest.mark.parametrize(
+    "batch_size_per_device, act_dtype, weight_dtype",
+    ((1, ttnn.bfloat16, ttnn.bfloat16),),
+)
+@pytest.mark.parametrize(
+    "resolution",
+    [
+        (320, 320),
+        (640, 640),
+    ],
+)
+def test_e2e_performant_dp(
+    mesh_device,
+    batch_size_per_device,
+    act_dtype,
+    weight_dtype,
+    model_location_generator,
+    resolution,
+):
+    run_perf_e2e_yolov4(
+        mesh_device,
+        batch_size_per_device,
+        act_dtype,
+        weight_dtype,
+        model_location_generator,
+        resolution,
     )
